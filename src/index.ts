6359231a447a5c822ce875a307e4b90f1e40c780
alicezeus <59837479+alicezeus@users.noreply.github.com>
2020-01-16 16:57:59 +0000

    getting started

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const tips = document.querySelectorAll('.tip') as NodeListOf<HTMLDivElement>;
+const selectedTip = 3;
+let currentTip = 1;
+
+tips.forEach(tip => {
+    if (currentTip === selectedTip) {
+        tip.dataset.selected = 'true';
+    }
+    currentTip++;
+    tip.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+    const tipSelected = that.dataset.selected === 'true';
+
+    that.classList.toggle('selected');
+
+}
